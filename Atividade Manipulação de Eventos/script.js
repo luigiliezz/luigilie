78ed5d8dc128c70367cf3fd61b37dd5e3a8e733b
luigiliezz <luizantonioatercio@gmail.com>
2024-10-30 14:22:03 -0400

    oi

---
+++ Atividade Manipulação de Eventos/script.js
@@ -0,0 +1,10 @@
+let miniatura = document.querySelector("#miniatura");
+let imgAmpliada = document.querySelector('#imgAmpliada')
+
+
+function mudarImagem() {
+    imgAmpliada.setAttribute('src')
+
+}
+
+miniatura.addEventListener("click", mudarImagem)
