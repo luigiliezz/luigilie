8245a7cf37505b5f3485e97c430ae709ab4e0961
luigiliezz <luizantonioatercio@gmail.com>
2024-11-06 14:31:10 -0400

    oi

---
+++ projeto-2b/src/app/sobre/page.js
@@ -0,0 +1,10 @@
+export default function Sobre() {
+    return (
+        <main>
+            <h1> Sobre a Tae </h1>
+            <p> Tae é a lider e Main Vocalist do grupo sul coreano Gilr's Generation</p>
+        </main>
+    );
+}
+
+
